cc49c9abca57c2a095031a77f940783888bb6ad0
Harsh Maur <harshmaur@gmail.com>
2017-05-17 09:00:42 +0530

    also run an custom function supplied by user onBlur

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ const withValidations = BaseComponent => class extends Component {
   }
   checkValidations() {
     for (let i = 0; i < this.props.validations.length; i++) {
-      const value = this.props.validations[i](
-        this.props.value,
-        this.props.config
-      )
+      const value = this.props.validations[i](this.props.value, this.props.config)
       if (value) {
         this.setState({
           error: value
@@ -25,7 +22,10 @@ const withValidations = BaseComponent => class extends Component {
       <BaseComponent
         {...this.props}
         {...this.state}
-        onBlur={() => this.checkValidations()}
+        onBlur={() => {
+          this.checkValidations()
+          this.props.onBlur() // run any supplied function by user
+        }}
       />
     )
   }
